06511d959853e4373d43a6e36850e5a67ab13b3a
Sourciluss667 <qcormand@sourciluss.net>
2020-03-09 15:22:07 +0100

    Add Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,60 @@
 <template>
-  <div id="app">
-    <router-view v-if="auth" />
-    <Auth v-else />
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-/*  Color Palette
- *  #98c1d9 BG
- *  #345995
- *  #6969b3
- *  #4b244a
- *  #ca1551
- */
-import Auth from '@/views/Auth.vue'
+import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
+
   components: {
-    Auth
+    HelloWorld
   },
-  data () {
-    return {
-      auth: false
-    }
-  },
-  created () {
-    if (document.cookie.includes('auth=true')) {
-      this.auth = true
-    } else {
-      document.cookie = 'auth=false'
-    }
-  }
+
+  data: () => ({
+    //
+  })
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
