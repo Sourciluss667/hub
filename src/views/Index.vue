c9dcdabc661d35cef6545b66dd9b68ceb01b5dd4
Sourciluss667 <qcormand@sourciluss.net>
2020-03-09 18:11:31 +0100

    Good v1

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -4,9 +4,24 @@
       <v-icon>mdi-settings</v-icon>
     </v-btn>
 
-    <v-alert type="warning" class="settings-alert" v-if="settings">
-      No settings...
-    </v-alert>
+    <v-alert type="warning" class="settings-alert" v-if="settings">No settings...</v-alert>
+
+    <v-content class="content">
+      <v-card class="mx-auto inline-block" max-width="344" v-for="project in projects" :key="project.id">
+        <v-card-text>
+          <div>Created by <span style="font-style: italic;">{{ project.creator }}</span></div>
+          <v-card-title class="title font-weight-medium" style="color: purple;">{{ project.name }}</v-card-title>
+          <p>{{ project.lastEdit.getDate() }}/{{ project.lastEdit.getMonth() }}/{{ project.lastEdit.getFullYear() }}</p>
+          <div class="text--primary">
+            {{ project.description }}
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text color="deep-purple accent-4" style="width: 100%;" v-on:click="go(project.link)">GO</v-btn>
+        </v-card-actions>
+      </v-card>
+
+    </v-content>
   </div>
 </template>
 
@@ -16,7 +31,8 @@ export default {
   name: 'Index',
   data () {
     return {
-      settings: false
+      settings: false,
+      projects: {}
     }
   },
   methods: {
@@ -25,6 +41,19 @@ export default {
       setTimeout(() => {
         this.settings = false
       }, 5000)
+    },
+    go (link) {
+      window.location.href = link
+    }
+  },
+  created () {
+    this.projects.pps = {
+      id: 1,
+      name: 'Pierre-Papier-Ciseaux',
+      creator: 'Sourciluss',
+      lastEdit: new Date(),
+      description: 'Un jeu de Pierre Papier Ciseaux...',
+      link: '#'
     }
   }
 }
@@ -43,5 +72,13 @@ export default {
   width: 95%;
   left: 50%;
   transform: translateX(-50%);
+}
+
+.content {
+display: block;
+position: absolute;
+width: 100%;
+height: 90%;
+bottom: 0;
 }
 </style>
